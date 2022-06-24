4a38e6f2f349cac39f7f6f3275b780fc89f59a65
qkzk <qu3nt1n@gmail.com>
2022-06-24 15:35:27 +0200

    UI. Refactoring. Use Reactive[type], typing. Setter/Getter for childs

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -14,16 +14,17 @@ from ..model import Issue
 from ..tokens import REPOS_DICT
 from .colors import COLORS
 
+LEFT_BUTTON = 1
+RIGHT_BUTTON = 3
+
 
 class IssueView(Widget):
     is_repo_selected: Reactive[bool] = Reactive(False)
     text_color: Reactive[str] = Reactive("grey70")
+    selected_issue_color: Reactive[str]
 
     def on_mount(self) -> None:
-        self.text_color = "grey70"
         self.set_interval(1, self.refresh)
-        self.is_repo_selected = False
-        self.selected_color: str
 
     def render(self) -> Text:
         return Text(
@@ -33,35 +34,37 @@ class IssueView(Widget):
             no_wrap=True,
         )
 
-    def set_issue(self, issue: Issue):
+    def with_issue(self, issue: Issue) -> "IssueView":
         self.issue = issue
         return self
 
-    def set_selected(self):
-        self.text_color = self.selected_color + " bold italic"
-
-    def set_repo_selected(self):
-        self.is_repo_selected = True
-        self.text_color = "grey70 bold"
-
-    def set_repo_not_selected(self):
-        self.is_repo_selected = False
-        self.text_color = "grey70"
-
-    def set_selected_color(self, color):
-        self.selected_color = color
+    def with_selected_color(self, color: str) -> "IssueView":
+        self.selected_issue_color = color
         return self
+
+    def set_selected(self) -> None:
+        self.text_color = self.selected_issue_color + " bold italic"
+
+    def set_repo_selected(self) -> None:
+        self.is_repo_selected = True
+        self.text_color = "grey90 bold"
+
+    def set_repo_not_selected(self) -> None:
+        self.is_repo_selected = False
+        self.text_color = "grey50"
 
 
 class IssueDetail(Widget):
     is_repo_selected: Reactive[bool] = Reactive(False)
+    border_style: Reactive[str]
+    issue: Reactive[Issue]
 
-    def on_mount(self):
-        self.issue: Issue
-        self.border_style: str
-
-    def set_issue(self, issue: Issue):
+    def with_issue(self, issue: Issue) -> "IssueDetail":
         self.issue = issue
+        return self
+
+    def with_border_style(self, color: str) -> "IssueDetail":
+        self.border_style = color
         return self
 
     def render(self) -> Panel:
@@ -69,76 +72,80 @@ class IssueDetail(Widget):
             self.issue.body,
             title=self.issue.title,
             subtitle=self.issue.labels_str,
-            style="grey100 bold" if self.is_repo_selected else "grey100",
+            style=self.color,
             border_style=self.border_style,
         )
 
-    def set_repo_selected(self):
+    @property
+    def color(self) -> str:
+        return "grey100 bold" if self.is_repo_selected else "grey100"
+
+    def set_repo_selected(self) -> None:
         self.is_repo_selected = True
 
-    def set_repo_not_selected(self):
+    def set_repo_not_selected(self) -> None:
         self.is_repo_selected = False
-
-    def set_color(self, color):
-        self.border_style = color
-        return self
 
 
 class RepoView(Widget):
     is_selected: Reactive[bool] = Reactive(False)
+    nb_issues: Reactive[int] = Reactive(0)
+    selected_index: Reactive[int] = Reactive(0)
+    has_issues: Reactive[bool] = Reactive(False)
+    index: Reactive[int]
 
     def on_mount(self) -> None:
         self.set_interval(1, self.refresh)
         self.set_interval(30, self.reload_repos)
         self.reload_repos()
-        self.selected_index = 0
-        self.is_selected = False
-        self.nb_issues = 0
-        self.has_issues = False
-        self.index: int
-        self.border_style: str
 
-    def set_index(self, index):
+    def set_index(self, index: int) -> None:
         self.index = index
-        self.border_style = self.calc_border_style()
 
-    def reload_repos(self):
+    def reload_repos(self) -> None:
         self.repo = fetch_repo(self._name)
 
-    def calc_border_style(self):
-        style = f"{COLORS[self.index % len(COLORS)]}"
-        return style
+    def generate_content(self) -> list:
+        self.nb_issues = len(self.repo)
+        if self.nb_issues == 0:
+            self.has_issues = False
+            return [" "]
+
+        self.has_issues = True
+        content: list[IssueView | IssueDetail]
+        content = [
+            IssueView().with_issue(issue).with_selected_color(self.border_style)
+            for issue in self.repo
+        ]
+        if self.is_selected:
+            self.select_all_views(content)
+        content[self.selected_index].set_selected()
+        content.append(self.generate_issue_detail())
+        return content
+
+    @staticmethod
+    def select_all_views(content: list[IssueView | IssueDetail]):
+        for view in content:
+            view.set_repo_selected()
+
+    def generate_issue_detail(self) -> IssueDetail:
+        return (
+            IssueDetail()
+            .with_issue(self.repo[self.selected_index])
+            .with_border_style(self.border_style)
+        )
+
+    @property
+    def border_style(self) -> str:
+        return f"{COLORS[self.index % len(COLORS)]}" + (
+            " bold italic" if self.is_selected else ""
+        )
 
     def render(self) -> Panel:
-        content: list[str | IssueView | IssueDetail]
-        self.nb_issues = len(self.repo)
-        if len(self.repo) > 0:
-            self.has_issues = True
-            content = [
-                IssueView().set_issue(issue).set_selected_color(self.border_style)
-                for issue in self.repo
-            ]
-            content.append(
-                IssueDetail()
-                .set_issue(self.repo[self.selected_index])
-                .set_color(self.border_style)
-            )
-            if self.is_selected:
-                for view in content:
-                    view.set_repo_selected()
-            content[self.selected_index].set_selected()
-        else:
-            content = [" "]
-            self.has_issues = False
-        border_style = (
-            self.border_style + " bold italic"
-            if self.is_selected
-            else self.border_style
-        )
         return Panel(
-            Columns(content),
+            Columns(self.generate_content()),
             title=self._name,
-            border_style=border_style,
+            border_style=self.border_style,
         )
 
     def set_name(self, name: str) -> None:
@@ -148,39 +155,41 @@ class RepoView(Widget):
         self._address = address
 
     def is_in_issues_list(self, y: int) -> bool:
-        return len(self.repo) > 1 and y <= len(self.repo)
+        return self.has_issues and y <= len(self.repo)
 
-    def select_issue(self, y: int):
+    def set_selected_issue_index(self, y: int) -> None:
         """
         must offset by one since repo indexes starts at 0 and line at 1.
         """
         self.selected_index = y - 1
 
-    def open_repo_in_browser(self):
+    def open_repo_in_browser(self) -> None:
         open_in_browser(self.repo.url)
 
-    def open_selected_issue_in_browser(self):
-        open_in_browser(self.repo[self.selected_index].url)
+    def open_selected_issue_in_browser(self) -> None:
+        if self.has_issues:
+            open_in_browser(self.repo[self.selected_index].url)
 
-    def open_repo_in_ranger(self):
+    def open_repo_in_ranger(self) -> None:
         open_in_ranger(self._address)
 
-    def open_selected_issue_in_lvim_octo(self):
-        open_in_lvim_octo(self._address, self.repo[self.selected_index].number)
+    def open_selected_issue_in_lvim_octo(self) -> None:
+        if self.has_issues:
+            open_in_lvim_octo(self._address, self.repo[self.selected_index].number)
 
     async def on_click(self, event: events.Click) -> None:
-        if event.button == 1:
+        if event.button == LEFT_BUTTON:
             if event.y == 0:
                 self.open_repo_in_browser()
             elif self.is_in_issues_list(event.y):
-                self.select_issue(event.y)
+                self.set_selected_issue_index(event.y)
             else:
                 self.open_selected_issue_in_browser()
-        if event.button == 3:
+        if event.button == RIGHT_BUTTON:
             if event.y == 0:
                 self.open_repo_in_ranger()
             elif self.is_in_issues_list(event.y):
-                self.select_issue(event.y)
+                self.set_selected_issue_index(event.y)
                 self.open_selected_issue_in_lvim_octo()
             else:
                 self.open_selected_issue_in_lvim_octo()
@@ -192,15 +201,21 @@ class RepoView(Widget):
     async def on_leave(self, _: events.Leave) -> None:
         self.is_selected = False
 
-    def set_selected(self):
+    def set_selected(self) -> None:
         self.is_selected = True
 
-    def set_not_selected(self):
+    def set_not_selected(self) -> None:
         self.is_selected = False
 
 
 class EZTKView(App):
     WIN_WIDTH = 28
+
+    repos: Reactive[dict[str, str]] = Reactive({})
+    nb_repos: Reactive[int] = Reactive(0)
+    repo_views: Reactive[dict[str, RepoView]] = Reactive({})
+    selected_repo: Reactive[int] = Reactive(0)
+    selected_view: Reactive[RepoView]
 
     async def on_load(self, _: events.Load) -> None:
         """Bind keys with the app loads (but before entering application mode)"""
@@ -223,18 +238,34 @@ class EZTKView(App):
         await self.bind("f4", "f4", "Open issue in lvim octo")
 
     async def on_mount(self, _: events.Mount) -> None:
-        self.set_repos()
+        self.set_defaults()
+        await self.set_grid()
 
-        self.repo_views: dict[str, RepoView] = {}
+    def set_defaults(self) -> None:
+        self.set_repos()
+        self.set_repo_views()
+        self.set_default_selected_repo()
+        self.set_nb_repos()
+
+    def set_repos(self) -> None:
+        self.repo_dict = REPOS_DICT
+
+    def set_nb_repos(self) -> None:
+        self.nb_repos = len(self.repo_dict)
+
+    def set_default_selected_repo(self) -> None:
+        self.selected_repo = 0
+
+    def set_repo_views(self) -> None:
+        self.repo_views = {}
         for index, (name, address) in enumerate(self.repo_dict.items()):
             repo_view = RepoView()
             repo_view.set_name(name)
             repo_view.set_address(address)
             repo_view.set_index(index)
             self.repo_views[name] = repo_view
-        await self.set_grid()
 
-    async def set_grid(self, height=29):
+    async def set_grid(self, height=29) -> None:
         self.grid = await self.view.dock_grid()
 
         self.grid.add_column("col", fraction=1, max_size=self.WIN_WIDTH)
@@ -250,74 +281,63 @@ class EZTKView(App):
             row.max_size = win_height
         self.refresh(layout=True)
 
-    def set_repos(self) -> None:
-        self.repo_dict = REPOS_DICT
-        self.nb_repos = len(self.repo_dict)
-        self.selected_repo = 0
-
-    def calc_height(self, width: int, height) -> int:
+    def calc_height(self, width: int, height: int) -> int:
         """
         Calculates the height of repo windows.
         """
-        max_win_per_row = width / self.WIN_WIDTH
-        if max_win_per_row >= self.nb_repos:
-            win_height = height - 1
-        elif max_win_per_row >= math.ceil(self.nb_repos / 2):
-            win_height = (height // 2) - 1
-        elif max_win_per_row >= math.ceil(self.nb_repos / 3):
-            win_height = (height // 3) - 1
-        else:
-            win_height = (height // 4) - 1
-        return win_height
+        return height // math.ceil(self.nb_repos * self.WIN_WIDTH / width) - 1
 
-    def get_selected_view(self) -> RepoView:
-        key = list(self.repo_dict.keys())[self.selected_repo]
-        return self.repo_views[key]
-
-    def set_selected_repo(self, index: int):
+    def select_view(self, index: int) -> None:
         key = list(self.repo_dict.keys())[index]
         view = self.repo_views[key]
         view.set_selected()
+        self.selected_view = view
 
-    def set_not_selected_repo(self, index: int):
+    def unselect_view(self, index: int) -> None:
         key = list(self.repo_dict.keys())[index]
         view = self.repo_views[key]
         view.set_not_selected()
 
     async def action_up(self, *_) -> None:
         """select previous issue in current"""
-        view = self.get_selected_view()
+        view = self.selected_view
         if view.has_issues:
             view.selected_index = (view.selected_index - 1) % view.nb_issues
         self.refresh()
 
     async def action_down(self, *_) -> None:
         """select next issue in current"""
-        view = self.get_selected_view()
+        view = self.selected_view
         if view.has_issues:
             view.selected_index = (view.selected_index + 1) % view.nb_issues
         self.refresh()
 
+    def decr_selected_repo(self) -> None:
+        self.selected_repo = (self.selected_repo - 1) % self.nb_repos
+
+    def incr_selected_repo(self) -> None:
+        self.selected_repo = (self.selected_repo + 1) % self.nb_repos
+
     async def action_left(self, *_) -> None:
         """Give focus to previous repo"""
-        self.set_not_selected_repo(self.selected_repo)
-        self.selected_repo = (self.selected_repo - 1) % self.nb_repos
-        self.set_selected_repo(self.selected_repo)
+        self.unselect_view(self.selected_repo)
+        self.decr_selected_repo()
+        self.select_view(self.selected_repo)
 
     async def action_right(self, *_) -> None:
         """Give focus to next repo"""
-        self.set_not_selected_repo(self.selected_repo)
-        self.selected_repo = (self.selected_repo + 1) % self.nb_repos
-        self.set_selected_repo(self.selected_repo)
+        self.unselect_view(self.selected_repo)
+        self.incr_selected_repo()
+        self.select_view(self.selected_repo)
 
     async def action_f1(self, *_) -> None:
-        self.get_selected_view().open_repo_in_browser()
+        self.selected_view.open_repo_in_browser()
 
     async def action_f2(self, *_) -> None:
-        self.get_selected_view().open_selected_issue_in_browser()
+        self.selected_view.open_selected_issue_in_browser()
 
     async def action_f3(self, *_) -> None:
-        self.get_selected_view().open_repo_in_ranger()
+        self.selected_view.open_repo_in_ranger()
 
     async def action_f4(self, *_) -> None:
-        self.get_selected_view().open_selected_issue_in_lvim_octo()
+        self.selected_view.open_selected_issue_in_lvim_octo()
